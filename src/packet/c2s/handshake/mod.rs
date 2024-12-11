7b3794d064f5d397ea749b6971acaecf2e3c91c5
Totobird <>
2024-12-10 23:19:41 -0500

    Moved handshake items up a level in the module tree;

--- src/packet/c2s/handshake/mod.rs
+++ src/packet/c2s/handshake/mod.rs
@@ -1,3 +1,18 @@
 use super::*;
 
-pub mod handshake;
+#[packet]
+#[derive(Debug)]
+pub struct HandshakeC2SPacket {
+    pub protocol_version : VarInt,
+    pub address          : String,
+    pub port             : u16,
+    pub intended_state   : ConnectionIntent,
+}
+
+#[packet(VarInt)]
+#[derive(Debug)]
+pub enum ConnectionIntent {
+    Status   = 0,
+    Login    = 1,
+    Transfer = 2,
+}
