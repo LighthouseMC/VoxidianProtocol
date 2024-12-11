ded6361888d569091e414eac2a099b974ad0849a
akarahdev <endisticakarah@gmail.com>
2024-12-11 09:33:42 -0500

    feat: make packet macro auto derive certain useful traits

--- src/packet/c2s/handshake/mod.rs
+++ src/packet/c2s/handshake/mod.rs
@@ -1,7 +1,6 @@
 use super::*;
 
 #[packet]
-#[derive(Debug)]
 pub struct HandshakeC2SPacket {
     pub protocol_version : VarInt,
     pub address          : String,
@@ -10,7 +9,6 @@ pub struct HandshakeC2SPacket {
 }
 
 #[packet(VarInt)]
-#[derive(Debug, PartialEq, Eq)]
 pub enum ConnectionIntent {
     Login    = 0,
     Status   = 1,
