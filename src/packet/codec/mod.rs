5c6eb3457baf9f055846cc8fd6bc5b90d39157c6
akarahdev <endisticbutitsforgit@gmail.com>
2024-12-10 22:07:58 -0500

    feat: fix various compilation & runtime bugs

--- src/packet/codec/mod.rs
+++ src/packet/codec/mod.rs
@@ -5,18 +5,5 @@ pub use decode::*;
 
 use super::{PacketBuf, VarInt};
 
-pub trait PacketEncodeDecode: Sized {
-    fn encode(&self, buf: &mut PacketBuf) -> Result<(), EncodeError>;
-
-    fn decode(buf: &mut PacketBuf) -> Result<Self, DecodeError>;
-}
-
-impl<T: PacketEncode + PacketDecode> PacketEncodeDecode for T {
-    fn encode(&self, buf: &mut PacketBuf) -> Result<(), EncodeError> {
-        PacketEncode::encode(self, buf)
-    }
-
-    fn decode(buf: &mut PacketBuf) -> Result<Self, DecodeError> {
-        PacketDecode::decode(buf)
-    }
+pub trait PacketEncodeDecode: Sized + PacketEncode + PacketDecode {
 }
