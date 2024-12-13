9872e4c43da3c11a44a265197d9f017b7cdb16af
Totobird <>
2024-12-13 00:16:05 -0500

    Added unformatted display for Text; Fixed empty Texts not encoding properly; Added reset and inheric methods for Text;

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,6 @@
 pub mod packet;
 pub mod value;
 pub mod registry;
+
+
+// TODO: Current packet uses protocol 767 (1.21.1). At some point, follow https://minecraft.wiki/w/Minecraft_Wiki:Projects/wiki.vg_merge/Pre-release_protocol to update to protocol 769 (1.21.4).
