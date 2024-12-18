52311e62200026e4d6aef4bfeee41713038e9a1f
akarahdev <endisticakarah@gmail.com>
2024-12-18 09:10:01 -0500

    feat: refactor packet dump system

--- voxidian-protocol-datagen/src/main/java/net/totobirdcreations/voxidianprotocoldatagen/mixin/MinecraftServerMixin.java
+++ voxidian-protocol-datagen/src/main/java/net/totobirdcreations/voxidianprotocoldatagen/mixin/MinecraftServerMixin.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonObject;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.Identifier;
+import net.totobirdcreations.voxidianprotocoldatagen.dumps.PacketDump;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,46 +23,9 @@ import java.lang.Exception;
 @Mixin(MinecraftServer.class)
 class MinecraftServerMixin {
 
-	@Unique
-	private static int i = 0;
-
 	@Inject(at = @At("HEAD"), method = "startServer")
 	private static <S extends MinecraftServer> void startServer(Function<Thread, S> factory, CallbackInfoReturnable<S> info) {
-		var packets_json = new JsonObject();
-
-		for (var packet_group : PacketGroup.values()) {
-			MinecraftServerMixin.i = 0;
-			packet_group.factory.forEachPacketType((type, ignored) -> {
-				var id = Identifier.of(
-					type.id().getNamespace(),
-					(
-						packet_group.bound.toString().toLowerCase()
-							+ "/" + packet_group.stage.toString().toLowerCase()
-							+ "/" + type.id().getPath()
-					)
-						.replace("initialize" , "initialise" )
-						.replace("level"      , "world"      )
-						.replace("center"     , "centre"     )
-				);
-				var packet_json = new JsonObject();
-				packet_json.add("prefix" , new JsonPrimitive(MinecraftServerMixin.i        ));
-				packet_json.add("bound"  , new JsonPrimitive(packet_group.bound.toString() ));
-				packet_json.add("stage"  , new JsonPrimitive(packet_group.stage.toString() ));
-				packets_json.add(id.toString(), packet_json);
-				MinecraftServerMixin.i += 1;
-			});
-		}
-
-		var gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
-		try {
-			var file = new FileWriter(new File(new File(new File(System.getProperty("user.dir")).getParentFile().getParentFile(), "generated"), "packets.json"));
-			gson.toJson(packets_json, file);
-			file.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-			return;
-		}
+		PacketDump.dump();
 		System.exit(0);
 	}
 
