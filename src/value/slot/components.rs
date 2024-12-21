76b3bbae1382152d85eb1e5f89f6d83258dff4c5
akarahdev <endisticbutitsforgit@gmail.com>
2024-12-21 10:52:24 -0500

    feat: some codec implementations for SlotData

--- src/value/slot/components.rs
+++ src/value/slot/components.rs
@@ -1,8 +1,25 @@
 
+use crate::value::PacketEncode;
+use crate::value::PacketDecode;
+use crate::value::PacketBuf;
+use crate::value::EncodeError;
+use crate::value::DecodeError;
 use crate::value::VarInt;
-use voxidian_protocol_macros::component;
+use voxidian_protocol_macros::{component, packet_part};
 
+#[derive(Clone)]
 #[component("minecraft:max_stack_size")]
+#[packet_part]
 pub struct MaxStackSize {
     pub max_stack_size: VarInt
+}
+
+#[derive(Debug, Clone)]
+pub enum DataComponent {
+    MaxStackSize(MaxStackSize)
+}
+
+#[derive(Debug, Clone)]
+pub enum DataComponentType {
+    MaxStackSize,
 }
--- src/value/slot/mod.rs
+++ src/value/slot/mod.rs
@@ -7,9 +7,52 @@ pub use components::*;
 #[derive(Debug)]
 pub struct SlotData {
     pub item_count: VarInt,
-    pub item_id: Option<VarInt>
+    pub item_id: RegEntry<Item>,
+    pub components: Vec<DataComponent>,
+    pub removed_components: Vec<DataComponentType>
 }
 
-pub trait ComponentData {
+impl PacketEncode for SlotData {
+    fn encode(&self, buf: &mut PacketBuf) -> Result<(), EncodeError> {
+        buf.encode_write(self.item_count)?;
+        if self.item_count.as_i32() < 0 {
+            return Ok(());
+        }
+
+        buf.encode_write(self.item_id)?;
+
+        buf.encode_write(VarInt::from(self.components.len()))?;
+        buf.encode_write(VarInt::from(self.removed_components.len()))?;
+
+        for component in &self.components {
+            // TODO: write component
+        }
+
+        for component in &self.removed_components {
+            // TODO: write removed component
+        }
+        todo!();
+
+        Ok(())
+    }
+}
+
+impl PacketDecode for SlotData {
+    fn decode(buf: &mut PacketBuf) -> Result<Self, DecodeError> {
+        let item_count = buf.read_decode::<VarInt>()?;
+        if item_count.as_i32() == 0 {
+            return Ok(SlotData {
+                item_count: VarInt::from(0),
+                item_id: unsafe { RegEntry::new_unchecked(0) },
+                components: Vec::new(),
+                removed_components: Vec::new(),
+            });
+        }
+
+        todo!()
+    }
+}
+
+pub trait ComponentData: PacketEncode + PacketDecode {
     const ID: u32;
 }
