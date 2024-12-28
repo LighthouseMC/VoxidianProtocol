682a376b425bc45c25141250e238f2316962eddf
akarahdev <endisticbutitsforgit@gmail.com>
2024-12-28 11:53:39 -0500

    feat: rip out old datagen system

--- src/value/item.rs
+++ src/value/item.rs
@@ -1,5 +1,4 @@
 use super::*;
-use voxidian_protocol_macros::import_item_registry_from_file;
 
 #[derive(Debug)]
 pub struct Item {
@@ -9,23 +8,5 @@ pub struct Item {
 impl Item {
     pub fn from_identifier(id: Identifier) -> Item {
         Item { id }
-    }
-
-    pub fn vanilla_registry() -> Registry<Item> {
-        import_item_registry_from_file!("./generated/items.json")
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    pub fn vanilla_reg_test() {
-        let rg = Item::vanilla_registry();
-        assert!(rg.get(&Identifier::new("minecraft", "diamond")).is_some());
-        assert!(rg.get(&Identifier::new("minecraft", "diamond_sword")).is_some());
-        assert!(rg.get(&Identifier::new("minecraft", "dsjhkfhwejkew")).is_none());
-        assert_eq!(rg.make_entry(&Identifier::new("minecraft", "conduit")).unwrap().id(), 643);
     }
 }
