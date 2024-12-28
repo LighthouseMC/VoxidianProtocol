2511659deb4390ecd3ea0b5622a2d600697d32cc
akarahdev <endisticbutitsforgit@gmail.com>
2024-12-28 12:08:23 -0500

    feat: create datagen crate

--- voxidian-protocol-datagen/src/main.rs
+++ voxidian-protocol-datagen/src/main.rs
@@ -1,3 +1,15 @@
+extern crate voxidian_protocol;
+
+use std::collections::HashMap;
+
+mod generators;
+
 fn main() {
     println!("Hello, world!");
+}
+
+trait DataGenerator {
+    type Output;
+
+    fn generate(file_contents: &str) -> HashMap<String, Self::Output>;
 }
