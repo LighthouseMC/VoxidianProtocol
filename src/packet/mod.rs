bb1c4def8547162614e18ecec63a03dbb8bc85a1
akarahdev <endisticbutitsforgit@gmail.com>
2024-12-31 23:19:52 -0500

    feat: revert last commit, add pub to more fields

--- src/packet/mod.rs
+++ src/packet/mod.rs
@@ -39,7 +39,7 @@ pub(crate) macro packet_flags(
 
     #[derive(Debug, Clone, PartialEq, Eq)]
     $vis struct $ident { $(
-        $vis $fieldident : bool
+        $fieldvis $fieldident : bool
     ),* }
     impl PacketEncode for $ident { fn encode(&self, buf : &mut PacketBuf) -> Result<(), EncodeError> {
         <$type as PacketEncode>::encode(
