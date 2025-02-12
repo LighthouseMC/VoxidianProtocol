db382f8b4addd048efe9885008c433ca92ea6f99
Totobird <45902300+Totobird-Creations@users.noreply.github.com>
2025-02-12 17:47:13 -0500

    Added OpenScreenS2CPlayPacket;

--- src/packet/s2c/play/mod.rs
+++ src/packet/s2c/play/mod.rs
@@ -548,7 +548,39 @@ pub enum Hand {
 }
 
 #[packet("minecraft:s2c/play/open_screen")]
-pub struct OpenScreenS2CPlayPacket(TODO);
+pub struct OpenScreenS2CPlayPacket {
+    pub window : VarInt,
+    pub kind   : ScreenWindowKind,
+    pub title  : NbtText
+}
+#[packet_part(VarInt)]
+pub enum ScreenWindowKind {
+    Generic9x1   = 0,
+    Generic9x2   = 1,
+    Generic9x3   = 2,
+    Generic9x4   = 3,
+    Generic9x5   = 4,
+    Generic9x6   = 5,
+    Generic3x3   = 6,
+    Crafter3x3   = 7,
+    Anvil        = 8,
+    Beacon       = 9,
+    BlastFurnace = 10,
+    BrewingStand = 11,
+    Crafting     = 12,
+    Enchantment  = 13,
+    Furnace      = 14,
+    Grindstone   = 15,
+    Hopper       = 16,
+    Lectern      = 17,
+    Loom         = 18,
+    Merchant     = 19,
+    ShulkerBox   = 20,
+    Smithing     = 21,
+    Smoker       = 22,
+    Cartography  = 23,
+    Stonecutter  = 24
+}
 
 #[packet("minecraft:s2c/play/open_sign_editor")]
 pub struct OpenSignEditorS2CPlayPacket {
