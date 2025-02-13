c1220819623d84e7073502b549ff1aeaca89f3e6
akarahdev <endisticbutitsforgit@gmail.com>
2025-02-13 09:06:56 -0500

    feat: block states are now sorted to allow for any order if inputs

--- src/autogenerated/block_states.rs
+++ src/autogenerated/block_states.rs
@@ -14,7 +14,8 @@ pub static ID_TO_BLOCK_STATE: LazyLock<HashMap<i32, BlockState>> = LazyLock::new
     let mut map = HashMap::new();
 
     for state in BLOCK_STATES.iter() {
-        map.insert(state.protocol_id, state.block_state.clone());
+        let state2 = state.block_state.clone().sorted();
+        map.insert(state.protocol_id, state2);
     }
 
     map
@@ -24,7 +25,7 @@ pub static BLOCK_STATE_TO_ID: LazyLock<HashMap<BlockState, i32>> = LazyLock::new
     let mut map = HashMap::new();
 
     for state in BLOCK_STATES.iter() {
-        map.insert(state.block_state.clone(), state.protocol_id, );
+        map.insert(state.block_state.clone().sorted(), state.protocol_id);
     }
 
     map
@@ -39,7 +40,22 @@ impl BlockState {
         ID_TO_BLOCK_STATE.get(&id).cloned()
     }
 
+    pub fn sort(&mut self) {
+        self.properties.sort_by_cached_key(|x| x.0.clone());
+    }
+
+    pub fn sorted(mut self) -> Self {
+        self.sort();
+        self
+    }
+
     pub fn to_id(&self) -> Option<i32> {
+        let state = self.clone().sorted();
+        BLOCK_STATE_TO_ID.get(&state).cloned()
+    }
+
+    pub fn to_id_mut(&mut self) -> Option<i32> {
+        self.sort();
         BLOCK_STATE_TO_ID.get(self).cloned()
     }
 }
--- src/packet/buffer.rs
+++ src/packet/buffer.rs
@@ -1,34 +1,33 @@
 use super::*;
-use std::{ fmt, iter, slice, vec };
-use std::io::{ self, Error, ErrorKind, Read, Write };
 use flate2::read::GzDecoder;
-
+use std::io::{self, Error, ErrorKind, Read, Write};
+use std::{fmt, iter, slice, vec};
 
 #[derive(Clone, Hash, Eq, PartialEq)]
 pub struct PacketBuf {
-    inner    : Vec<u8>,
-    read_idx : usize,
+    inner: Vec<u8>,
+    read_idx: usize,
 }
-
 
 /// Constructors
 impl PacketBuf {
-
     pub fn new() -> Self {
         PacketBuf {
-            inner    : Vec::new(),
-            read_idx : 0,
+            inner: Vec::new(),
+            read_idx: 0,
         }
     }
 
-    pub fn of_encode<T : PacketEncode>(encodable : T) -> Result<Self, EncodeError> {
+    pub fn of_encode<T: PacketEncode>(encodable: T) -> Result<Self, EncodeError> {
         let mut buf = Self::new();
         encodable.encode(&mut buf)?;
         Ok(buf)
     }
 
     /// Also returns the number of bytes that were consumed.
-    pub fn from_raw_queue(mut queue : impl Iterator<Item = u8>) -> Result<(Self, usize), DecodeError> {
+    pub fn from_raw_queue(
+        mut queue: impl Iterator<Item = u8>,
+    ) -> Result<(Self, usize), DecodeError> {
         let (size, consumed) = VarInt::decode_iter(&mut queue)?;
         let size = size.as_i32() as usize;
         let mut bytes = Vec::with_capacity(size);
@@ -38,10 +37,13 @@ impl PacketBuf {
             };
             bytes.push(byte);
         }
-        Ok((Self {
-            inner : bytes,
-            read_idx : 0
-        }, consumed + size))
+        Ok((
+            Self {
+                inner: bytes,
+                read_idx: 0,
+            },
+            consumed + size,
+        ))
     }
 
     pub fn inflate_gzip(&self) -> io::Result<Self> {
@@ -50,41 +52,44 @@ impl PacketBuf {
     }
 
     /// Reads all bytes from the stream, returning it as a `PacketBuf`.
-    pub fn read(f : &mut impl Read) -> io::Result<Self> {
+    pub fn read(f: &mut impl Read) -> io::Result<Self> {
         let mut bytes = Vec::new();
         f.read_to_end(&mut bytes)?;
         Ok(Self {
-            inner : bytes,
-            read_idx : 0
+            inner: bytes,
+            read_idx: 0,
         })
     }
-
 }
 impl From<Vec<u8>> for PacketBuf {
-    fn from(value : Vec<u8>) -> Self { Self {
-        inner    : value,
-        read_idx : 0
-    } }
+    fn from(value: Vec<u8>) -> Self {
+        Self {
+            inner: value,
+            read_idx: 0,
+        }
+    }
 }
-
 
 /// Deconstructors
 impl PacketBuf {
-
     pub fn into_inner(self) -> Vec<u8> {
-        self.inner.into_iter().skip(self.read_idx).collect::<Vec<_>>()
+        self.inner
+            .into_iter()
+            .skip(self.read_idx)
+            .collect::<Vec<_>>()
     }
 
     pub fn as_slice(&self) -> &[u8] {
         &self.inner.as_slice().get(self.read_idx..).unwrap_or(&[])
     }
-
 }
 
 impl Read for PacketBuf {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         for idx in 0..buf.len() {
-            buf[idx] = self.read_u8().map_err(|e| Error::from(ErrorKind::UnexpectedEof))?;
+            buf[idx] = self
+                .read_u8()
+                .map_err(|_| Error::from(ErrorKind::UnexpectedEof))?;
         }
         Ok(buf.len())
     }
@@ -105,15 +110,14 @@ impl Write for PacketBuf {
 
 /// Basic Operations
 impl PacketBuf {
-
     #[track_caller]
-    pub fn seek(&mut self, idx : usize) {
+    pub fn seek(&mut self, idx: usize) {
         assert!(idx <= self.inner.len(), "Seek index exceeded packet size");
         self.read_idx = idx;
     }
 
     #[track_caller]
-    pub fn skip(&mut self, count : usize) {
+    pub fn skip(&mut self, count: usize) {
         self.seek(self.read_idx + count);
     }
 
@@ -121,32 +125,44 @@ impl PacketBuf {
         self.inner.len() - self.read_idx
     }
 
-    pub fn write_u8(&mut self, byte : u8) {
+    pub fn write_u8(&mut self, byte: u8) {
         self.inner.push(byte);
     }
 
-    pub fn write_u8s(&mut self, data : &[u8]) {
+    pub fn write_u8s(&mut self, data: &[u8]) {
         self.inner.extend_from_slice(data);
     }
 
     pub fn read_u8(&mut self) -> Result<u8, DecodeError> {
-        let byte = self.inner
-            .get(self.read_idx).ok_or(DecodeError::EndOfBuffer)?;
+        let byte = self
+            .inner
+            .get(self.read_idx)
+            .ok_or(DecodeError::EndOfBuffer)?;
         self.read_idx += 1;
         Ok(*byte)
     }
 
-    pub fn read_u8s_const<const BYTES : usize>(&mut self) -> Result<[u8; BYTES], DecodeError> {
-        if (BYTES == 0) { return Ok([0; BYTES]); }
-        let out = self.inner.iter().skip(self.read_idx).cloned().array_chunks()
-            .next().ok_or(DecodeError::EndOfBuffer)?;
+    pub fn read_u8s_const<const BYTES: usize>(&mut self) -> Result<[u8; BYTES], DecodeError> {
+        if (BYTES == 0) {
+            return Ok([0; BYTES]);
+        }
+        let out = self
+            .inner
+            .iter()
+            .skip(self.read_idx)
+            .cloned()
+            .array_chunks()
+            .next()
+            .ok_or(DecodeError::EndOfBuffer)?;
         self.read_idx += BYTES;
         Ok(out)
     }
 
-    pub fn read_u8s(&mut self, bytes : usize) -> Result<Vec<u8>, DecodeError> {
-        if (bytes == 0) { return Ok(Vec::new()) }
-        let mut out  = Vec::with_capacity(bytes);
+    pub fn read_u8s(&mut self, bytes: usize) -> Result<Vec<u8>, DecodeError> {
+        if (bytes == 0) {
+            return Ok(Vec::new());
+        }
+        let mut out = Vec::with_capacity(bytes);
         let mut data = self.inner.iter().skip(self.read_idx);
         for _ in 0..bytes {
             out.push(*data.next().ok_or(DecodeError::EndOfBuffer)?);
@@ -156,22 +172,20 @@ impl PacketBuf {
     }
 
     #[deprecated = "Are you sure?"]
-    pub(crate) fn insert(&mut self, idx : usize, bytes : &[u8]) {
+    pub(crate) fn insert(&mut self, idx: usize, bytes: &[u8]) {
         self.inner.splice(idx..idx, bytes.iter().cloned());
     }
-
 }
 
-
 impl<'l> IntoIterator for &'l PacketBuf {
-    type Item     = &'l u8;
+    type Item = &'l u8;
     type IntoIter = iter::Skip<slice::Iter<'l, u8>>;
     fn into_iter(self) -> Self::IntoIter {
         self.inner.iter().skip(self.read_idx)
     }
 }
 impl IntoIterator for PacketBuf {
-    type Item     = u8;
+    type Item = u8;
     type IntoIter = iter::Skip<vec::IntoIter<u8>>;
     fn into_iter(self) -> Self::IntoIter {
         self.inner.into_iter().skip(self.read_idx)
@@ -179,23 +193,21 @@ impl IntoIterator for PacketBuf {
 }
 /// Iterator
 impl PacketBuf {
-    pub fn iter(&self) -> impl Iterator<Item = u8> { (&self).into_iter().map(|byte| *byte) }
+    pub fn iter(&self) -> impl Iterator<Item = u8> {
+        (&self).into_iter().map(|byte| *byte)
+    }
 }
-
 
 /// Encode & Decode
 impl PacketBuf {
-
-    pub fn encode_write<T : PacketEncode>(&mut self, encodable : T) -> Result<(), EncodeError> {
+    pub fn encode_write<T: PacketEncode>(&mut self, encodable: T) -> Result<(), EncodeError> {
         encodable.encode(self)
     }
 
-    pub fn read_decode<T : PacketDecode>(&mut self) -> Result<T, DecodeError> {
+    pub fn read_decode<T: PacketDecode>(&mut self) -> Result<T, DecodeError> {
         T::decode(self)
     }
-
 }
-
 
 impl fmt::Debug for PacketBuf {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -207,29 +219,37 @@ impl fmt::Debug for PacketBuf {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn varint_decode_iter() {
-        let data = [16, 0, 129, 6, 9, 108, 111, 99, 97, 108, 104, 111, 115, 116, 99, 221, 1, 1, 0];
+        let data = [
+            16, 0, 129, 6, 9, 108, 111, 99, 97, 108, 104, 111, 115, 116, 99, 221, 1, 1, 0,
+        ];
         //          ^Packet length
-        let Ok((len, consumed)) = VarInt::decode_iter(&mut data.into_iter()) else { panic!("decode_iter was not a success"); };
+        let Ok((len, consumed)) = VarInt::decode_iter(&mut data.into_iter()) else {
+            panic!("decode_iter was not a success");
+        };
         assert_eq!(len.as_i32(), 16);
         assert_eq!(consumed, 1);
     }
 
     #[test]
     fn packetbuf_from_raw_queue() {
-        let data = [16, 0, 129, 6, 9, 108, 111, 99, 97, 108, 104, 111, 115, 116, 99, 221, 1, 1, 0];
+        let data = [
+            16, 0, 129, 6, 9, 108, 111, 99, 97, 108, 104, 111, 115, 116, 99, 221, 1, 1, 0,
+        ];
         //          |   |------------------------------------------------------------------  |  ^Status request packet
         //          |   ^Handshake packet                                                    ^Packet length
         //          ^Packet length
-        let Ok((packetbuf, consumed)) = PacketBuf::from_raw_queue(data.into_iter()) else { panic!("from_raw_queue was not a success") };
-        assert_eq!(packetbuf.inner, [0, 129, 6, 9, 108, 111, 99, 97, 108, 104, 111, 115, 116, 99, 221, 1]);
+        let Ok((packetbuf, consumed)) = PacketBuf::from_raw_queue(data.into_iter()) else {
+            panic!("from_raw_queue was not a success")
+        };
+        assert_eq!(packetbuf.inner, [
+            0, 129, 6, 9, 108, 111, 99, 97, 108, 104, 111, 115, 116, 99, 221, 1
+        ]);
         assert_eq!(consumed, 17);
     }
-
 }
