7cccad832dea28579ee5425f8a69c84b6fbc90ee
akarahdev <endisticbutitsforgit@gmail.com>
2025-03-11 22:23:52 -0400

    feat: BlockState is now BTreeMap-driven, since block properties are canonically unordered

--- src/value/block_state.rs
+++ src/value/block_state.rs
@@ -1,11 +1,22 @@
+use std::collections::{BTreeMap};
+
 use super::*;
 
-#[derive(Debug, Hash, Eq, PartialEq, Serialize, Deserialize, Clone)]
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize, Clone)]
 pub struct BlockState {
     pub id: Identifier,
-    pub properties: Vec<(String, String)>
+    pub properties: BTreeMap<String, String>
 }
 
+impl std::hash::Hash for BlockState {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+        for property in &self.properties {
+            property.0.hash(state);
+            property.1.hash(state);
+        }
+    }
+}
 
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -13,3 +24,4 @@ pub struct BlockStateWithMetadata {
     pub block_state: BlockState,
     pub protocol_id: i32
 }
+
